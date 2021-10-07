fa81be0b8795a9d99122fcfd66e217ba29b7b07e
andrearosasco <andrea.rosasco@iit.it>
2021-10-07 11:08:31 +0200

    fixed datetime bug (again)

--- ours/configs/cfg1.py
+++ ours/configs/cfg1.py
@@ -51,5 +51,5 @@ class TrainConfig:
     n_epoch = 20
     log_metrics_every = 1
     log_pcs_every = 10000
-    seed = datetime.now().timestamp()   # 1234 5678 does not converge
+    seed = int(datetime.now().timestamp())   # 1234 5678 does not converge
     num_workers = 40
