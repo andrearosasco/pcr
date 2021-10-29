fixed point cloud logging, parameters loggin, mesh rotation in main

ours/configs/server_config.py
@@ -12,7 +12,7 @@ device = 'cuda'
 class DataConfig:
     dataset_path = "../data/ShapeNetCore.v2"
     partial_points = 2024 # TODO Possible OOM
-    multiplier_complete_sampling = 20
+    multiplier_complete_sampling = 50
     noise_rate = 0.02  # amount of noise added to the point sampled on the mesh
     percentage_sampled = 0.1  # number of uniformly sampled points
     mode = 'easy'  # values: train, valid, test
@@ -23,7 +23,7 @@ class DataConfig:
 @dataclass
 class ModelConfig:
     PC_SIZE = 2048
-    knn_layer = 3 
+    knn_layer = 3 # TODO Prima era 1
     device = device
     # Transformer
     n_channels = 3
@@ -32,7 +32,7 @@ class ModelConfig:
     mlp_ratio = 2.
     qkv_bias = False
     num_heads = 6
-    attn_drop_rate = 0.2 # TODO non stiamo usando il dropout da nessuna parte?
+    attn_drop_rate = 0.2 # TODO non stiamo usando il dropout da nessuna parte? Prima era a 0
     drop_rate = 0.2
     qk_scale = None
     out_size = 1024
@@ -57,14 +57,14 @@ class TrainConfig:
     lr = 1e-4
     mb_size = 64 # TODO perché serve così basso adesso?
     test_mb_size = 32
-    n_epoch = 1
+    n_epoch = 20
     clip_value = 5 # 0.5?
     log_metrics_every = 100
     seed = 1   # 1234 5678 does not converge int(datetime.now().timestamp())
     # WARNING: Each worker load a different batches so we may end up with
     #   20 * 64 batches loaded simultaneously. Moving the batches to cuda inside the
     #   dataset can lead to OOM errors
-    num_workers = 20
+    num_workers = 10
     git = git_hash()
     optimizer = torch.optim.Adam
     loss = torch.nn.BCEWithLogitsLoss

ours/datasets/ShapeNetPOVDepth.py
@@ -143,7 +143,7 @@ class ShapeNet(data.Dataset):
         samples = torch.tensor(samples).float()
         occupancy = torch.tensor(occupancy, dtype=torch.float) / 255
 
-        return label, partial_pcd, [str(complete_path), mean, var], samples, occupancy
+        return label, partial_pcd, [str(complete_path), rotation, mean, var], samples, occupancy
 
     def __len__(self):
         return int(self.n_samples)

ours/datasets/ShapeNetPOVRemoval.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import torch.utils.data as data
 import open3d as o3d
+from open3d.cpu.pybind.visualization import draw_geometries
 from torch.utils.data import DataLoader
 
 o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel(0))
@@ -13,7 +14,7 @@ from scipy.spatial.transform import Rotation as R
 
 
 class ShapeNet(data.Dataset):
-    def __init__(self, config, mode="train", overfit_mode=False):
+    def __init__(self, config, mode="easy/train", overfit_mode=False):
         self.mode = mode
         self.overfit_mode = overfit_mode
         #  Backbone Input
@@ -56,6 +57,10 @@ class ShapeNet(data.Dataset):
             complete_pcd = mesh.sample_points_uniformly(self.partial_points * self.multiplier_complete_sampling)
             _, pt_map = complete_pcd.hidden_point_removal([0, 0, dist], 1000)  # radius * 4
             partial_pcd = complete_pcd.select_by_index(pt_map)
+            #
+            # partial_pcd.paint_uniform_color([0, 0, 1])
+            # complete_pcd.paint_uniform_color([1, 0, 0])
+            # draw_geometries([partial_pcd, complete_pcd])
 
             if len(np.array(partial_pcd.points)) != 0:
                 break
@@ -101,7 +106,7 @@ class ShapeNet(data.Dataset):
         samples = torch.tensor(samples).float()
         occupancy = torch.tensor(occupancy, dtype=torch.float) / 255
 
-        return label, partial_pcd, [str(complete_path), mean, var], samples, occupancy
+        return label, partial_pcd, [str(complete_path), rotation, mean, var], samples, occupancy
 
     def __len__(self):
         return int(self.n_samples)

ours/main.py
@@ -2,6 +2,10 @@ import math
 import os
 import sys
 
+from open3d.cpu.pybind.utility import Vector3dVector
+from open3d.cpu.pybind.visualization import draw_geometries
+from open3d.cpu.pybind.geometry import PointCloud
+
 from configs import DataConfig, ModelConfig, TrainConfig
 os.environ['CUDA_VISIBLE_DEVICES'] = TrainConfig.visible_dev
 from pytorch_lightning.callbacks import GPUStatsMonitor, ProgressBar, ModelCheckpoint, ProgressBarBase
@@ -21,7 +25,7 @@ from models.HyperNetwork import BackBone, ImplicitFunction
 import torch
 from tqdm import tqdm
 import copy
-from utils.misc import create_3d_grid, check_mesh_contains
+from utils.misc import create_3d_grid, check_mesh_contains, create_cube
 import open3d as o3d
 import wandb
 
@@ -71,7 +75,7 @@ class HyperNetwork(pl.LightningModule):
         self.training_set = ShapeNet(DataConfig,
                                      mode=f"{DataConfig.mode}/train",
                                      overfit_mode=TrainConfig.overfit_mode)
-        print(len(self.training_set))
+
         self.valid_set = ShapeNet(DataConfig,
                                   mode=f"{DataConfig.mode}/valid",
                                   overfit_mode=TrainConfig.overfit_mode)
@@ -86,7 +90,6 @@ class HyperNetwork(pl.LightningModule):
                           worker_init_fn=seed_worker,
                           generator=generator)
 
-        print(len(self.training_set), '/', TrainConfig.mb_size, '=', len(dl))
         return dl
 
     def val_dataloader(self):
@@ -183,30 +186,51 @@ class HyperNetwork(pl.LightningModule):
         self.log('valid/loss', self.avg_loss)
         self.log('valid_step', self.current_epoch)
 
-        output = output[0]
-        pred, trgt, mesh = output['out'][0], output['target'][0], output['mesh'][0]
+        idxs = [np.random.randint(0, len(output)), -1]
 
-        # all positive predictions with labels for true positive and false positives
-        precision_pc = torch.cat((self.grid[0].cpu(), trgt), dim=-1).detach().cpu().numpy()
-        precision_pc = precision_pc[(pred.cpu().numpy() == 1).squeeze()]
+        for idx, name in zip(idxs, ['fixed', 'random']):
+            batch = output[idx]
+            out, trgt, mesh = batch['out'][0], batch['target'][0], batch['mesh'][0]
+            pred = out > 0.5
 
-        # all true points with labels for true positive and false negatives
-        recall_pc = torch.cat((self.grid[0].cpu(), pred), dim=-1).detach().cpu().numpy()
-        recall_pc = recall_pc[(trgt.cpu().numpy() == 1.).squeeze()]
+            # all positive predictions with labels for true positive and false positives
+            precision_pc = torch.cat((self.grid[0].cpu(), trgt), dim=-1).detach().cpu().numpy()
+            precision_pc = precision_pc[pred.squeeze()]
 
-        complete = o3d.io.read_triangle_mesh(mesh[0], False)
+            # all true points with labels for true positive and false negatives
+            recall_pc = torch.cat((self.grid[0].cpu(), pred.int()), dim=-1).detach().cpu().numpy()
+            recall_pc = recall_pc[(trgt == 1.).squeeze()]
 
-        complete = complete.sample_points_uniformly(10000)
-        complete = np.array(complete.points)
+            complete = o3d.io.read_triangle_mesh(mesh[0], False)
 
-        partial = output['partial']
+            complete = complete.sample_points_uniformly(10000)
+            complete = np.array(complete.points)
 
-        self.trainer.logger.experiment[0].log({
-            'precision_pc': wandb.Object3D({"points": precision_pc, 'type': 'lidar/beta'}),
-            'recall_pc': wandb.Object3D({"points": recall_pc, 'type': 'lidar/beta'}),
-            'partial_pc': wandb.Object3D({"points": partial, 'type': 'lidar/beta'}),
-            'complete_pc': wandb.Object3D({"points": complete, 'type': 'lidar/beta'})
-        })
+            partial = batch['partial']
+            partial = partial.squeeze()
+
+            self.trainer.logger.experiment[0].log({
+                f'{name}_precision_pc': wandb.Object3D({"points": precision_pc, 'type': 'lidar/beta'}),
+                f'{name}_recall_pc': wandb.Object3D({"points": recall_pc, 'type': 'lidar/beta'}),
+                f'{name}_partial_pc': wandb.Object3D({"points": partial, 'type': 'lidar/beta'}),
+                f'{name}_complete_pc': wandb.Object3D({"points": complete, 'type': 'lidar/beta'})
+            })
+
+def visualize(meshes, partials):
+    mesh_paths, rotations, means, vars = meshes
+    p, r, m, v = mesh_paths[0], rotations[0], means[0], vars[0]
+
+
+    mesh1 = o3d.io.read_triangle_mesh(p, False)
+    mesh2 = copy.deepcopy(mesh1)
+    mesh1.rotate(r.cpu().numpy())
+    mesh1.translate(-m.cpu().numpy())
+    mesh1.scale(1 / (v.cpu().numpy() * 2), center=[0, 0, 0])
+
+    pc = PointCloud()
+    pc.points = Vector3dVector(partials[0].cpu().numpy())
+
+    draw_geometries([mesh1, mesh2, pc, create_cube()])
 
 def print_memory():
     import gc
@@ -223,12 +247,13 @@ def print_memory():
 
 
 def chamfer(samples, predictions, meshes):
-    mesh_paths, means, vars = meshes
-    for p, m, v, pred in zip(mesh_paths, means, vars, predictions):
+    mesh_paths, rotations, means, vars = meshes
+    for p, r, m, v, pred in zip(mesh_paths, rotations, means, vars, predictions):
         query = samples[0, (pred > 0.5).squeeze()]
 
         scene = o3d.t.geometry.RaycastingScene()
         mesh = o3d.io.read_triangle_mesh(p, False)
+        mesh.rotate(r.cpu().numpy())
         mesh.translate(-m.cpu().numpy())
         mesh.scale(1 / (v.cpu().numpy() * 2), center=[0, 0, 0])
         mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)
@@ -244,8 +269,17 @@ if __name__ == '__main__':
     # model.to('cuda')
     # print_memory()
 
-    wandb_logger = WandbLogger(project='pcr', log_model='all', entity='coredump')
-    wandb_logger.watch(model)
+
+    config = {'train': {k: dict(TrainConfig.__dict__)[k] for k in dict(TrainConfig.__dict__) if
+                             not k.startswith("__")},
+              'model': {k: dict(ModelConfig.__dict__)[k] for k in dict(ModelConfig.__dict__) if
+                             not k.startswith("__")},
+              'watch': {k: dict(DataConfig.__dict__)[k] for k in dict(DataConfig.__dict__) if
+                            not k.startswith("__")}}
+
+    wandb_logger = WandbLogger(project='pcr', log_model='all', entity='coredump', config=config)
+
+    wandb_logger.watch(model, log='all', log_freq=TrainConfig.log_metrics_every)
 
 
     checkpoint_callback = ModelCheckpoint(

ours/utils/misc.py
@@ -7,8 +7,12 @@ import torch.nn.functional as F
 import os
 from collections import abc
 import tqdm
-from open3d.cuda.pybind.geometry import PointCloud
-from open3d.cuda.pybind.utility import Vector3dVector
+try:
+    from open3d.cuda.pybind.geometry import PointCloud
+    from open3d.cuda.pybind.utility import Vector3dVector
+except ModuleNotFoundError:
+    from open3d.cpu.pybind.geometry import PointCloud
+    from open3d.cpu.pybind.utility import Vector3dVector
 #from pytorch3d.ops import sample_points_from_meshes
 #from pytorch3d.loss.point_mesh_distance import point_face_distance
 #from pytorch3d.renderer import look_at_view_transform, Materials, PointLights, BlendParams
@@ -457,10 +461,11 @@ def create_3d_grid(min_value=-0.5, max_value=0.5, step=0.04, batch_size=1):
 def check_mesh_contains(meshes, queries, max_dist=0.01):
     occupancies = []
     queries = queries.detach().cpu().numpy()
-    mesh_paths, means, vars = meshes
-    for p, m, v, query in zip(mesh_paths, means, vars, queries):
+    mesh_paths, rotations, means, vars = meshes
+    for p, r, m, v, query in zip(mesh_paths, rotations, means, vars, queries):
         scene = o3d.t.geometry.RaycastingScene()
         mesh = o3d.io.read_triangle_mesh(p, False)
+        mesh.rotate(r.cpu().numpy())
         mesh.translate(-m.cpu().numpy())
         mesh.scale(1 / (v.cpu().numpy() * 2), center=[0, 0, 0])
         mesh = o3d.t.geometry.TriangleMesh.from_legacy(mesh)