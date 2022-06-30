corrected normalization, added probability experiment

datasets/GraspingDataset.py
@@ -51,6 +51,7 @@ class GraspingDataset(Dataset):
         x2_pose = np.load(((self.root / partial_path).parent / f'_0_{p2}_0_model_pose.npy').as_posix())
         y3_pose = np.load(((self.root / partial_path).parent / f'_{p3}_0_0_model_pose.npy').as_posix())
 
+
         vertices = match_mesh_to_partial(np.array(vertices), [y1_pose, x2_pose, y3_pose])
         mesh = TriangleMesh(vertices=Vector3dVector(vertices), triangles=Vector3iVector(triangles))
         complete = np.array(mesh.sample_points_uniformly(8192 * 2).points)
@@ -68,7 +69,8 @@ class GraspingDataset(Dataset):
             partial = np.concatenate([partial, zeros])
 
         center = get_bbox_center(complete)
-        diameter = np.sqrt(np.max(np.sum((complete) ** 2, axis=1))) * 2
+        # diameter = np.sqrt(np.max(np.sum((complete - center) ** 2, axis=1))) * 2
+        diameter = get_diameter(complete - center)
 
         complete, partial = (complete - center) / diameter, (partial - center) / diameter
 
@@ -223,7 +225,7 @@ def get_bbox_center(pc):
 
 def get_diameter(pc):
     diameter = pc.max(0) - pc.min(0)
-    return diameter
+    return np.max(diameter)
 
 
 class deterRandomSampler(Sampler):

model/Decoder.py
@@ -5,7 +5,10 @@ from torch.optim import Adam
 from configs import Config
 
 class Decoder:
-    def __init__(self, sdf):
+    def __init__(self, sdf, num_points, thr, itr):
+        self.num_points = num_points
+        self.thr = thr
+        self.itr = itr
 
         self.sdf = sdf
 
@@ -15,7 +18,7 @@ class Decoder:
             self.sdf.eval()
 
             batch_size = fast_weights[0][0].shape[0]
-            refined_pred = torch.tensor(torch.randn(batch_size, 20_000, 3).cpu().detach().numpy() * 0.1, device=Config.General.device,
+            refined_pred = torch.tensor(torch.randn(batch_size, self.num_points, 3).cpu().detach().numpy() * 1, device=Config.General.device,
                                         requires_grad=True)
 
             loss_function = BCEWithLogitsLoss(reduction='mean')
@@ -24,11 +27,13 @@ class Decoder:
             c1, c2, c3, c4 = 1, 0, 0, 0 #1, 0, 0  1, 1e3, 0 # 0, 1e4, 5e2
             new_points = [[] for _ in range(batch_size)]
             # refined_pred.detach().clone()
-            for step in range(20):
+            for step in range(self.itr):
                 results = self.sdf(refined_pred, fast_weights)
 
                 for i in range(batch_size):
-                    new_points[i] += [refined_pred[i].detach().clone()[(torch.sigmoid(results[i]).squeeze() >= 0.70), :]]
+                    points = refined_pred[i].detach().clone()[(torch.sigmoid(results[i]).squeeze() >= self.thr), :]
+                    preds = torch.sigmoid(results[i]).detach().clone()[torch.sigmoid(results[i]).squeeze() >= self.thr]
+                    new_points[i] += [torch.cat([points, preds], dim=1)]
 
                 gt = torch.ones_like(results[..., 0], dtype=torch.float32)
                 gt[:, :] = 1
@@ -42,13 +47,13 @@ class Decoder:
                 optim.step()
 
         selected = [torch.cat(points).squeeze() for points in new_points]
-        res = torch.zeros([batch_size, 8192*2, 3], device=Config.General.device)
+        res = torch.zeros([batch_size, self.num_points, 4], device=Config.General.device)
         for i, s in enumerate(selected):
             # torch.sum(torch.sum(torch.cat([s > 0.5, s < -0.5], dim=1), dim=1) != 0)
-            k = min(s.size(0), 8192*2)
+            k = min(s.size(0), self.num_points)
             perm = torch.randperm(s.size(0))
             res[i][:k] = s[perm[:k]]
 
         self.sdf.train(old)
-        return res
+        return res[..., :3], res[..., -1]
 

model/PCRNetwork2.py
@@ -37,7 +37,7 @@ class PCRNetwork(pl.LightningModule, ABC):
         super().__init__()
         self.backbone = BackBone(config)
         self.sdf = ImplicitFunction(config)
-        self.decoder = Decoder(self.sdf)
+        self.decoder = Decoder(self.sdf, 8192*2, 0.7, 20) #  8192*2, 0.7, 20
         self.train_decoder = Decoder2(self.sdf)
 
         # self.apply(self._init_weights)
@@ -123,12 +123,10 @@ class PCRNetwork(pl.LightningModule, ABC):
         return [dl1, dl2]
 
     def forward(self, partial, object_id=None, step=0.01):
-        samples = create_3d_grid(batch_size=partial.shape[0], step=step).to(Config.General.device)
-
         fast_weights, _ = self.backbone(partial)
-        prediction = torch.sigmoid(self.sdf(samples, fast_weights))
+        pc, _ = self.decoder(fast_weights)
 
-        return samples[prediction.squeeze(-1) > 0.5], fast_weights
+        return pc
 
     def configure_optimizers(self):
         optimizer = Config.Train.optimizer(self.parameters(), lr=Config.Train.lr, weight_decay=Config.Train.wd)
@@ -220,7 +218,7 @@ class PCRNetwork(pl.LightningModule, ABC):
                                                              noise_rate=Config.Data.noise_rate,
                                                              tolerance=Config.Data.tolerance)
                 fast_weights, _ = self.backbone(partial)
-                pc1 = self.decoder(fast_weights)
+                pc1, _ = self.decoder(fast_weights)
                 out2 = torch.sigmoid(self.sdf(samples2, fast_weights))
                 self.val_outputs = {'random': {'pc1': pc1, 'out2': out2.detach().squeeze(2).cpu(),
                                                'target2': occupancy2.detach().cpu(),
@@ -236,7 +234,7 @@ class PCRNetwork(pl.LightningModule, ABC):
                                                              noise_rate=Config.Data.noise_rate,
                                                              tolerance=Config.Data.tolerance)
                 fast_weights, _ = self.backbone(partial)
-                pc1 = self.decoder(fast_weights)
+                pc1, _ = self.decoder(fast_weights)
                 out2 = torch.sigmoid(self.sdf(samples2, fast_weights))
                 self.val_outputs['fixed'] = {'pc1': pc1, 'out2': out2.detach().squeeze(2).cpu(),
                                              'target2': occupancy2.detach().cpu(), 'samples2': samples2.detach().cpu(),
@@ -244,7 +242,7 @@ class PCRNetwork(pl.LightningModule, ABC):
                                              'batch_idx': batch_idx, 'dl_idx': dl_idx}
 
             if (self.current_epoch + 1) % 10 != 0:
-                return {'dl_idx': dl_idx}
+                return
 
 
         # The sampling with "sample_point_cloud" simulate the sampling used during training
@@ -278,35 +276,22 @@ class PCRNetwork(pl.LightningModule, ABC):
                     fast_weights = [[fast_weights[i], fast_weights[i + 1], fast_weights[i + 2]] for i in
                                     range(0, 3 * (Config.Model.depth + 2), 3)]
 
-        pc1 = self.decoder(fast_weights)
+        pc1, prob = self.decoder(fast_weights)
         out2 = torch.sigmoid(self.sdf(samples2, fast_weights))
 
-        return {'pc1': pc1, 'out2': out2.detach().squeeze(2).cpu(),
-                'target2': occupancy2.detach().cpu(), 'samples2': samples2.detach().cpu(),
-                'partial': partial.detach().cpu(), 'ground_truth': ground_truth, 'batch_idx': batch_idx, 'dl_idx': dl_idx}
 
-    def validation_step_end(self, output):
-        if (self.current_epoch + 1) % 10 != 0:
-            if output['dl_idx'] == 1:
-                self.trainer._active_loop.outputs = []
-                return
-
-        pred, trgt = output['out2'], output['target2'].int()
+        pred, trgt = out2.detach().squeeze(2).cpu(), occupancy2.detach().cpu().int()
         pred = torch.nan_to_num(pred)
 
-        if output['dl_idx'] == 0:
+        if dl_idx == 0:
             metrics = self.metrics['val_views']
-        if output['dl_idx'] == 1:
+        if dl_idx == 1:
             metrics = self.metrics['val_models']
 
         metrics['accuracy'](pred, trgt), metrics['precision'](pred, trgt)
-        metrics['chamfer'](chamfer_batch_pc(output['pc1'], output['ground_truth']).cpu())
+        metrics['chamfer'](chamfer_batch_pc(pc1, ground_truth).cpu())
         metrics['recall'](pred, trgt), metrics['f1'](pred, trgt)
         metrics['loss'](F.binary_cross_entropy(pred, trgt.float()))
-
-        output['pc1'], output['ground_truth'] = output['pc1'].detach().cpu(), output['ground_truth'].detach().cpu()
-
-        self.trainer._active_loop.outputs = []
 
     def validation_epoch_end(self, output):
 